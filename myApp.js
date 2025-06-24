e56a8b30f046a9d363a59bedc516d6c9b779a068
Emmanuel Leo Demby <emmanuelleosamuel203@gmail.com>
2025-06-24 11:46:09 -0400

    update 4

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const app = express();
+dotenv.config();
 
 console.log("Hello World");
 
@@ -9,7 +11,8 @@ const absolutePath = __dirname + `/views/index.html`;
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/json', function(req, res) {
-    res.json({"message": "Hello json"});
+    const greeting = "Hello json";
+    res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? greeting.toUpperCase() : greeting});
   })
  
 
