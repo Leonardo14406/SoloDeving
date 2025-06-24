9e5dcf6d0f157e08f99522bad272ccaa7d623333
Emmanuel Leo Demby <emmanuelleosamuel203@gmail.com>
2025-06-24 11:56:40 -0400

    updated

--- myApp.js
+++ myApp.js
@@ -7,12 +7,15 @@ console.log("Hello World");
 
 const absolutePath = __dirname + `/views/index.html`;
 
-
+const response = "Hello json".toUpperCase();
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/json', function(req, res) {
-    const greeting = "Hello json";
-    res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? greeting.toUpperCase() : greeting});
+   if (process.env.MESSAGE_STYLE  === "uppercase") {
+        res.json({ "message": response });
+   } else {
+    res.json("Hello json");
+   }
   })
  
 
