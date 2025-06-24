aedf03e22eb2cf615ed52a6ad3f7e1a9f9344936
Emmanuel Leo Demby <emmanuelleosamuel203@gmail.com>
2025-06-24 11:28:00 -0400

    update 3

--- myApp.js
+++ myApp.js
@@ -8,8 +8,8 @@ const absolutePath = __dirname + `/views/index.html`;
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-    res.sendFile(absolutePath);
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"});
   })
  
 
