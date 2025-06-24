e2446d8931fb3de54750de0f27830a4db2a41aaa
Emmanuel Leo Demby <emmanuelleosamuel203@gmail.com>
2025-06-24 11:16:15 -0400

    updated 2

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log("Hello World");
 const absolutePath = __dirname + `/views/index.html`;
 
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
     res.sendFile(absolutePath);
